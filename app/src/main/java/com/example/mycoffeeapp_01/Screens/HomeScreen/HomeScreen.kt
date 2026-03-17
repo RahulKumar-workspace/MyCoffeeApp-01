dac59b03e6ba89372442eaaea196e63664aa0be8
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-03-17 14:36:34 +0530

    Added Bottom Nav Bar & Started Building Home Screen

--- app/src/main/java/com/example/mycoffeeapp_01/Screens/HomeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/mycoffeeapp_01/Screens/HomeScreen/HomeScreen.kt
@@ -1,17 +1,89 @@
 package com.example.mycoffeeapp_01.Screens.HomeScreen
 
+import androidx.compose.material3.Icon
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mycoffeeapp_01.Screens.UiComponents.BottomNavBar
+import androidx.compose.ui.unit.dp
+import com.example.mycoffeeapp_01.Screens.UiComponents.MyBottomNavBar
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.sp
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun HomeScreen() {
-
     Scaffold(
-        bottomBar = {
-            BottomNavBar()
+        bottomBar = { MyBottomNavBar() }
+    ) { innerPadding ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(fraction = 1f / 3f)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color(0xFF303030),
+                            Color(0xFF1F1F1F),
+                            Color(0xFF121212)
+                        )
+                    )
+                )
+        )
+
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .padding(innerPadding)
+        ) {
+            Text(text = "Location",
+                color = Color.Gray,
+                fontSize = 16.sp,
+
+                )
+
+            Row() {
+                Text(text = "hi",
+                    color = Color.White)
+                Icon(imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Change Location",
+                    tint = Color.White
+                )
+            }
         }
-    ) { }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/mycoffeeapp_01/Screens/UiComponents/MyBottomNavBar.kt
+++ app/src/main/java/com/example/mycoffeeapp_01/Screens/UiComponents/MyBottomNavBar.kt
@@ -3,12 +3,12 @@ package com.example.mycoffeeapp_01.Screens.UiComponents
 import androidx.compose.material3.Icon
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Label
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,45 +17,51 @@ import androidx.compose.ui.unit.dp
 import com.example.mycoffeeapp_01.R
 import androidx.compose.ui.res.painterResource
 import com.example.mycoffeeapp_01.ui.theme.LightBrown
+import androidx.compose.material3.Label
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun BottomNavBar() {
+fun MyBottomNavBar() {
+
     // BottomNavItems
     val navItems = listOf(
-        NavItems(title = "Home", R.drawable.regular_outline_home),
-        NavItems(title = "Cart", R.drawable.regular_outline_bag),
-        NavItems(title = "Favorite", R.drawable.regular_outline_heart),
-        NavItems(title = "Profile", R.drawable.outline_account_circle_24)
+        NavItems("Home", R.drawable.regular_outline_home),
+        NavItems("Cart", R.drawable.regular_outline_bag),
+        NavItems("Favourites", R.drawable.regular_outline_heart),
+        NavItems("Profile", R.drawable.outline_account_circle_24)
     )
 
     NavigationBar(
-        contentColor = MaterialTheme.colorScheme.surface,
-        modifier = Modifier.height(100.dp),
+        containerColor = MaterialTheme.colorScheme.surface,
+        modifier = Modifier.height(100.dp)
     ) {
         navItems.forEachIndexed { index, items ->
             NavigationBarItem(
                 icon = {
                     Icon(
-                        painter = painterResource(id = items.icon),
-                        contentDescription = items.title
+                        painter = painterResource(items.icon),
+                        contentDescription = "Home"
                     )
                 },
-                label = { Text(items.title) },
+                label = { Text(text = items.title) },
                 modifier = Modifier.size(30.dp),
-                onClick = { },
+                onClick = { /*TODO*/ },
                 selected = true,
                 alwaysShowLabel = false,
                 colors = NavigationBarItemDefaults.colors(
                     selectedIconColor = LightBrown,
                     selectedTextColor = LightBrown,
-                    unselectedIconColor = Color.DarkGray,
                     unselectedTextColor = Color.DarkGray,
-                    indicatorColor = LightBrown.copy(alpha = 0.02f)
+                    unselectedIconColor = Color.DarkGray,
+                    indicatorColor = LightBrown.copy(alpha = 0.03f)
+
                 )
             )
+
         }
     }
+
+
 }
 
 
