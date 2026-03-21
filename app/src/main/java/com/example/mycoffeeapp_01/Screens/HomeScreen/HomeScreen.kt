24b2608d234a7f47e1502f27bc635d573a32b758
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-03-21 16:20:34 +0530

    Added Search Bar in Come Screen

--- app/src/main/java/com/example/mycoffeeapp_01/Screens/HomeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/mycoffeeapp_01/Screens/HomeScreen/HomeScreen.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -21,11 +23,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mycoffeeapp_01.Screens.UiComponents.MyBottomNavBar
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun HomeScreen() {
+
+    var location = "Jalandhar RD, Palarivttom"
+
     Scaffold(
         bottomBar = { MyBottomNavBar() }
     ) { innerPadding ->
@@ -52,18 +59,30 @@ fun HomeScreen() {
         ) {
             Text(text = "Location",
                 color = Color.Gray,
-                fontSize = 16.sp,
+                fontSize = 14.sp,
 
                 )
 
-            Row() {
-                Text(text = "hi",
-                    color = Color.White)
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = location,
+                    color = Color.White,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 16.sp
+                    )
                 Icon(imageVector = Icons.Default.KeyboardArrowDown,
                     contentDescription = "Change Location",
                     tint = Color.White
                 )
             }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+
+            MySearchBar()
         }
     }
 }
