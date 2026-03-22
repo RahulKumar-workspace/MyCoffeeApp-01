3556bd8b35e9076e5304e58455a7900be2dbe1d1
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-03-22 12:45:42 +0530

    Made A Card to add the products on the home page

---
+++ app/src/main/java/com/example/mycoffeeapp_01/Screens/HomeScreen/ProductsGrid.kt
@@ -0,0 +1,11 @@
+package com.example.mycoffeeapp_01.Screens.HomeScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun ProductsGrid() {
+    
+
+}
