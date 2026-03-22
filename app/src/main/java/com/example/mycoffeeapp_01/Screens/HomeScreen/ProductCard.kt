64e1595096dc317c147896c3faf67e4a1a4eecf7
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-03-22 12:45:36 +0530

    Made A Card to add the products on the home page

---
+++ app/src/main/java/com/example/mycoffeeapp_01/Screens/HomeScreen/ProductCard.kt
@@ -0,0 +1,122 @@
+package com.example.mycoffeeapp_01.Screens.HomeScreen
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mycoffeeapp_01.R
+import com.example.mycoffeeapp_01.ui.theme.IvoryWhite
+import com.example.mycoffeeapp_01.ui.theme.LightBrown
+
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+fun ProductCard(modifier: Modifier = Modifier) {
+
+    Card(
+        modifier = Modifier
+            .width(300.dp)
+            .padding(8.dp),
+        shape = RoundedCornerShape(16.dp),
+
+        ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.coffee_1),
+                    contentDescription = "Product Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(24.dp))
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Coffee Name",
+                style = typography.titleMedium.copy(
+                    color = Color.Black, fontWeight = FontWeight.SemiBold
+                )
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = "Coffee Description",
+                style = typography.bodySmall.copy(
+                    color = Color.Gray,
+                ),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Product Prict",
+                    style = typography.titleMedium.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = LightBrown
+                    )
+                )
+
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier.background(
+                        color = LightBrown,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add to Cart",
+                        tint = IvoryWhite
+                    )
+                }
+            }
+
+
+        }
+    }
+}
